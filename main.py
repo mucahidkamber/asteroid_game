9e75aeab2e5a946bd46d93609c17b323dbe3f29e
Mücahid Kamber <38380519+mucahidkamber@users.noreply.github.com>
2025-07-23 19:49:13 +0300

    Add Group class to manage multiple game objects

--- main.py
+++ main.py
@@ -12,6 +12,9 @@ def main():
     clock = pygame.time.Clock()
     dt = 0
 
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
@@ -19,10 +22,11 @@ def main():
             if event.type == pygame.QUIT:
                 return
 
-        player.update(dt)
+        updatable.update(dt)
 
         screen.fill("black")
-        player.draw(screen)
+        for item in drawable:
+            item.draw(screen)
         pygame.display.flip()
 
         dt = clock.tick(60) / 1000
